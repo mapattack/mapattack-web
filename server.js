fb6b41efa7a23c7809da81a78843e0c8e5c1d33e
Nate Goldman <nnmgoldman@gmail.com>
2013-09-19 15:39:04 -0700

    add twitter login w/ passport

--- server.js
+++ server.js
@@ -1,27 +1,97 @@
-var ecstatic = require('ecstatic');
-var flatiron = require('flatiron');
-var app = flatiron.app;
-var port = process.env.PORT;
+var pkg = require('./package.json');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 
-app.use(flatiron.plugins.http, {
-  before: [
-    function (req, res) {
-      var found = app.router.dispatch(req, res);
-      if (!found) {
-        res.emit('next');
-      }
-    },
-    ecstatic({
-      root: __dirname + '/app'
-    })
-  ],
-  after: []
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+var TWITTER_CONSUMER_KEY = 'VSCJzgLBienoPOs45tgYRQ';
+var TWITTER_CONSUMER_SECRET = 'CnMscq17Btt8cMuOTm7NQd72DYDoo676YUxm7L3fA';
+
+app
+  .set('port', port)
+  .set('views', __dirname + '/views')
+  .set('view engine', 'ejs')
+  .use(express.favicon())
+  .use(express.bodyParser())
+  .use(express.methodOverride())
+  .use(express.cookieParser('loqisaur'))
+  .use(express.session({secret : '#unicorner'}))
+  .use(passport.initialize())
+  .use(passport.session())
+  .configure('development', function(){
+    app
+      .use(express.logger('dev'))
+      .use(express.errorHandler({ dumpExceptions: true, showStack: true }))
+  })
+  .configure('production', function(){
+    app
+      .use(express.logger())
+      .use(express.errorHandler())
+  });
+
+app.use(express.static(__dirname + '/app'))
+app.use(app.router);
+
+/* twitter auth */
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
-app.router.get('/v', function () {
-  this.res.writeHead(200, { 'Content-Type': 'text/plain' });
-  this.res.end('flatiron ' + flatiron.version);
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
 });
 
-app.start(port);
-console.log('Flatiron started on port ' + port);
+passport.use(new TwitterStrategy({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callbackURL: 'http://127.0.0.1:3000/auth/twitter/callback'
+  },
+  function(token, tokenSecret, profile, done) {
+    process.nextTick(function () {
+      // To keep the example simple, the user's Twitter profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Twitter account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+function ensureAuthenticated(req, res, next) {
+  console.log('ensuring', req.isAuthenticated());
+  if (req.isAuthenticated()) { return next(); }
+  res.sendfile('index.html', {root: './app'});
+}
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/fail' }),
+  function(req, res) {
+    res.redirect('/login');
+  }
+);
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(app.get('port'), function(){
+  var started = [
+    pkg.name,
+    pkg.version,
+    'serving at',
+    'http://localhost:' + app.get('port'),
+    'in',
+    app.settings.env,
+    'mode'
+  ];
+  console.log(started.join(' '));
+});
