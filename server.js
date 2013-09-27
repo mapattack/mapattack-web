556fe3f339d0008c552cb80cd29173709f763bbb
Nate Goldman <nnmgoldman@gmail.com>
2013-09-27 16:39:14 -0700

    remove user console log

--- server.js
+++ server.js
@@ -98,7 +98,6 @@ function ensureAuthenticated(req, res, next) {
 }
 
 function loadAuthentication(req, res, next) {
-  console.log(req.user);
   res.locals({
     user: req.user ? req.user : null,
     authenticated: req.isAuthenticated()
